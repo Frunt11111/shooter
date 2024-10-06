5a07599c98a63450d51a6e2ea68a97783d03439f
Frunt11111 <frunt592@gmail.com>
2024-10-06 13:28:02 +0300

    Add files via upload
    Scripts, Player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from scripts.player import Player
 
 flags = pygame.RESIZABLE | pygame.SCALED
 window = pygame.display.set_mode((800, 600), flags)
@@ -8,9 +9,16 @@ FPS = 60
 
 background = pygame.image.load('images\\background.png')
 background = pygame.transform.scale(background, (800, 600))
+
 bullet = pygame.image.load('images\\bullet.png')
+
 enemy = pygame.image.load('images\\enemy.png')
-player = pygame.image.load('images\\player.png')
+
+player_image = pygame.image.load('images\\player.png')
+player_image = pygame.transform.scale(player_image, (50, 70))
+player = Player(player_image, 400, 550, 3)
+
+
 
 
 running = True
@@ -19,17 +27,15 @@ while running:
         if i.type == pygame.QUIT:
             running = False
 
-
-
-   #for enemy in enemies:
-     #   enemy.update()
-
+    #for enemy in enemies:
+    #enemy.update()
+    player.update()
+    #bullet.render()
 
     window.blit(background, (0, 0))
 
-    #bullet.render()
 
-    #player.render()
+    player.render(window)
 
     #for enemy in enemies:
        # enemy.render()
